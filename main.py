c7484a967492fe2dc1f5dc80bcc2f1a7f29789e6
Oluwakemi Vandy <kemivandy@gmail.com>
2022-05-31 11:51:25 +0100

    second commit

--- main.py
+++ main.py
@@ -9,25 +9,33 @@ while True:
  player=player.upper()
  list=["R","P","S"]
  CPU = random.choice(list)
- print("CPU chose", CPU)
+ #print("CPU chose", CPU)
     
  if player in list:
      if player == CPU:
+        print("CPU chose:",CPU)
         print("Its a Tie!")
- if player == "R":
+
+ if player == "R":  
     if CPU == "P":
+         print("Player(Rock) : CPU(Paper)")
          print("Oops.. CPU Wins!","Paper","beats","Rock")
     elif CPU == "S":
-         print("You Win!")
+         print("Player(Rock) : CPU(Scissors)")
+         print("You Win!","Rock","beats","Scissors" )
  elif player =="P":
      if CPU == "S":
+         print("Player(Paper) : CPU(Scissors)")
          print("Oops..CPU Wins!","Scissors","beats","Paper")
      elif CPU=="R":
-         print("You win!")
+         print("Player(Paper) : CPU(Rock)")
+         print("You win!","Paper","beats","Rock" )
  elif player =="S":
      if CPU =="R":
-        print("Oops..CPU Wins","Scissors","beats","Rock")
+        print("Player(Scisors) : CPU(Rock)")
+        print("Oops..CPU Wins","Rock","beats","Scissors")
      elif CPU == "P":
-            print("You Win!")
+            print("Player(Scissors) : CPU(Paper)")
+            print("You Win!","Scissors","beats","Paper" )
  else:
     print("Error! Input another choice")
